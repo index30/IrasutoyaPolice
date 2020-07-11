a1eb124e2a756a18482f531e85559cd2db139a78
ranthy <realthingshakes25@gmail.com>
2020-07-11 23:59:33 +0900

    Make main

--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@ from pptx.enum.shapes import MSO_SHAPE_TYPE
 import sys
 
 def main(argv):
-    ppt_path = Path(Path.cwd().parent, "ppt_file", argv[0])
-    ppt_file = Presentation(ppt_path.as_posix())
+    ppt_file = Presentation(argv[0])
     for slide in ppt_file.slides:
-            for shape in slide.shapes:
-                if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-                    print(shape._pic.nvPicPr.cNvPr.get('descr'))
+        for shape in slide.shapes:
+            if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
+                print(shape._pic.nvPicPr.cNvPr.get('descr'))
                 
 if __name__ == "__main__":
     main(sys.argv[1:])
